ad1e5fb4a6f66e768529e3a68744cb0216c8e4f9
Christian Legge <christian@christianlegge.dev>
2026-02-27 10:37:21 -0500

    server island for 404, node integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()],
-	site: "https://christianlegge.dev",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-});
+  integrations: [icon()],
+  site: "https://christianlegge.dev",
 
+  vite: {
+      plugins: [tailwindcss()],
+	},
+
+  adapter: node({
+    mode: "standalone",
+  }),
+});
