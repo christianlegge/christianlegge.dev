2b2bded4ac40962c58a23771f29e479cf70e6099
Christian Legge <christian@christianlegge.dev>
2026-03-29 13:05:02 -0400

    wii checklist generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,25 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
+import NodeFetchCache, { MemoryCache } from "node-fetch-cache";
+
+export const fetch = NodeFetchCache.create({
+	cache: new MemoryCache({ ttl: 1000 * 60 * 60 }),
+});
+
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
-  site: "https://christianlegge.dev",
+	integrations: [icon()],
+	site: "https://christianlegge.dev",
 
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
 
-  adapter: node({
-    mode: "standalone",
-  }),
+	adapter: node({
+		mode: "standalone",
+	}),
 });
+
